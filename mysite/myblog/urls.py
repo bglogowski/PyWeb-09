3dcb7bb23a10c3395de43721f1665630e982143b
Bryan Glogowski <glog@delft.pdx>
2016-05-30 22:35:16 -0700

    added post submission form

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from myblog.views import stub_view
 from myblog.views import list_view
 from myblog.views import detail_view
+from myblog.views import add_post
 
 from myblog.feeds import LatestEntriesFeed
 
@@ -13,7 +14,10 @@ urlpatterns = [
     url(r'^posts/(?P<post_id>\d+)/$',
         detail_view,
         name='blog_detail'),
-     url(r'^latest/feed/$',
+    url(r'^latest/feed/$',
          LatestEntriesFeed(),
          name='rss_feed'),
+    url(r'^add/post$', 
+        add_post, 
+        name='blog_add_post'),
 ]
--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render
+from django.shortcuts import render, redirect, render_to_response, get_object_or_404
 from django.template import RequestContext, loader
 
 from myblog.models import Post
+from myblog.forms import PostForm, CommentForm
 
 
 def stub_view(request, *args, **kwargs):
@@ -41,4 +42,25 @@ def category_detail_view(request, category_id):
         raise Http404
     context = {'post': post}
     return render(request, 'detail.html', context)
+
+
+def add_post(request):
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.save()
+        return redirect(post)
+    return render_to_response('add_post.html', { 'form': form }, context_instance=RequestContext(request))
+
+
+def view_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    form = CommentForm(request.POST or None)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+        return redirect(request.path)
+    return render_to_response('blog_post.html', { 'post': post, 'form': form, }, context_instance=RequestContext(request))
 
